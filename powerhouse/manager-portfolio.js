31032d85cf9af41a2d475f4c5b88195a3b3522f2
NoneForNowButMaybeLater <86891440+NoneForNowButMaybeLater@users.noreply.github.com>
2024-05-12 07:45:40 -0400

    powerhouse-4.0

--- powerhouse/manager-portfolio.js
+++ powerhouse/manager-portfolio.js
@@ -4,33 +4,23 @@ const portfolios = [
         "projects": [
             {
                 "image":"images/portfolio/modern_contemporary_villa.webp",
-                "alt":"picture of a modern contemporary villa",
-                "name": "Modern Contemporary Villa"
+                "alt":"picture of a contemporary villa",
+                "name": "Contemporary Villa"
             },
             {
                 "image":"images/portfolio/urban_townhouse.webp",
-                "alt":"picture of an urban townhouse",
-                "name": "Urban Townhouse"
+                "alt":"picture of a modern urban townhouse",
+                "name": "Modern Urban Townhouse"
             },
             {
-                "image":"",
-                "alt":"",
-                "name": "This is the title of an exterior design project"
+                "image":"images/portfolio/colonial_revival_mansion.webp",
+                "alt":"picture of a colonial revival mansion",
+                "name": "Colonial Revival Mansion"
             },
             {
-                "image":"",
-                "alt":"",
-                "name": "This is the title of an exterior design project"
-            },
-            {
-                "image":"",
-                "alt":"",
-                "name": "This is the title of an exterior design project"
-            },
-            {
-                "image":"",
-                "alt":"",
-                "name": "This is the title of an exterior design project"
+                "image":"images/portfolio/mediterranean-inspired_mansion.webp",
+                "alt":"picture of a mediterranean-inspired mansion",
+                "name": "Mediterranean-Inspired Mansion"
             }
         ]
     },
@@ -43,29 +33,19 @@ const portfolios = [
                 "name": "Modern Contemporary Interior"
             },
             {
-                "image":"images/portfolio/classic_victorian_home_renovation.webp",
+                "image":"images/portfolio/classic_victorian_interior.webp",
                 "alt":"Picture of a classic victorian home interior",
                 "name": "Classic Victorian Home Renovation"
             },
             {
-                "image":"",
-                "alt":"",
-                "name": "This is the title of an interior design project"
+                "image":"images/portfolio/minimalist_zen_interior.webp",
+                "alt":"Picture of a minimlist zen interior",
+                "name": "Minimalist Zen Interior"
             },
             {
-                "image":"",
-                "alt":"",
-                "name": "This is the title of an interior design project"
-            },
-            {
-                "image":"",
-                "alt":"",
-                "name": "This is the title of an interior design project"
-            },
-            {
-                "image":"",
-                "alt":"",
-                "name": "This is the title of an interior design project"
+                "image":"images/portfolio/art_deco_inspired_lounge.webp",
+                "alt":"picture of an art deco-inspired lounge",
+                "name": "Art Deco-inspired Lounge"
             }
         ]
     }

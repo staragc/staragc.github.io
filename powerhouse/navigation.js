f5b9af540220281009a1182395047b6f982ffec3
NoneForNowButMaybeLater <86891440+NoneForNowButMaybeLater@users.noreply.github.com>
2024-05-21 00:51:53 -0400

    powerhouse reset

--- powerhouse/navigation.js
+++ powerhouse/navigation.js
@@ -1,19 +1,45 @@
-const navBtn = document.getElementById("nav_btn");
-const navMenu = document.getElementById("nav_menu");
-const navBar = document.querySelector("nav");
-let navOn = false;
-//toggle nav bar
-navBtn.addEventListener("click", () =>{
-    if(navOn){
-        navOn = false;
-        navBtn.classList.remove("on");
-        navMenu.classList.remove("on");
-        navBar.classList.remove("on");
+/*desktop nav - start*/
+const desktopNav = document.getElementById("desktop_nav");
+//animate nav bar on scroll
+let lastScroll = window.scrollY;
+function hideDesktopNav(){
+    desktopNav.style.transform = "translateY(-100%)";
+}
+function showDesktopNav(){
+    desktopNav.style.transform = "translateY(0%)";
+}
+window.addEventListener("scroll", () =>{
+    if(window.scrollY <= lastScroll){
+        showDesktopNav();
     }
     else{
-        navOn = true;
-        navBtn.classList.add("on");
-        navMenu.classList.add("on");
-        navBar.classList.add("on");
+        hideDesktopNav();
     }
+    lastScroll = window.scrollY;
 })
+/*desktop nav - end*/
+
+
+/*mobile nav - start*/
+const mobileNavHeader = document.getElementById("mobile_nav_header");
+const mobileNav = document.getElementById("mobile_nav");
+const mobileNavBtn = document.getElementById("mnav_btn");
+let MNavOn = true;
+//toggle mobile nav
+function toggleMNav(){
+    if(MNavOn){
+        MNavOn = false;
+        mobileNavBtn.classList.remove("on");
+        mobileNavHeader.classList.remove("on");
+        mobileNav.classList.remove("on");
+    }
+    else{
+        MNavOn = true;
+        mobileNavBtn.classList.add("on");
+        mobileNavHeader.classList.add("on");
+        mobileNav.classList.add("on");
+    }
+}
+toggleMNav();
+mobileNavBtn.addEventListener("click", toggleMNav)
+/*mobile nav - end*/
